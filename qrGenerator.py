c146f5c9ae58416d1c76a620eafa33f4fdabac7f
Sudharshan Kumaresan <138243791+Sudharshank-cpu@users.noreply.github.com>
2024-09-18 12:21:45 +0530

    Added Comment Lines

--- qrGenerator.py
+++ qrGenerator.py
@@ -1,9 +1,17 @@
+#Importing QR Code Module(to Create) and Image Creating Module[PILLOW] (to Open)
 import qrcode
 from PIL import Image
-value=str(input("Enter String or Link: "))
+
+#Gathering Text from user to convert as QR Code Image File
+value = str(input("Enter String or Link: "))
+
+#Creates the QR Code
 qrImg=qrcode.make(value)
+
+#Saves as "qr-img.jpg" file, whrere near this file
 qrImg.save("qr-img.jpg")
 print("QR Code saved as 'qr-img.jpg'")
+
+#Opens the Created Image Automatically
 img=Image.open("qr-img.jpg")
 img.show()
-
